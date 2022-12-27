b924cbd1ce0d13a5c81c2c23a68187fe09d46ef4
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-27 12:34:57 +0530

    College-collection

---
+++ src/main/java/user/collection/UserApp.java
@@ -0,0 +1,32 @@
+package user.collection;
+
+import java.util.ArrayList;
+
+public class UserApp {
+    public static void main(String[] args) {
+//       ArrayList<User> users = createUser();
+//        User user1 = new User();
+//        user1.setUserId("ramesh123");
+//        user1.setName("Ramesh");
+//        user1.setRole("admin");
+//
+//        UserMachine userMachine = new UserMachine();
+//        userMachine.setLogicalName("SMWNODE101");
+//        userMachine.setName("First Floor Machine 1");
+//
+//        UserRole userRole = new UserRole();
+//        userRole.setName("operator");
+//
+////        User user2 = user.get(0);
+////        user2.getUserId();
+////        user2.getName();
+//    }
+//
+//    private static User createUser() {
+//        String [] servers = {"AdminServer1", "AdminServer2","AdminServer13"};
+//        User user = new User();
+//        user.setServers("Adminserver1");
+////        user.setServerPort("8080", "8086");
+//        return user;
+    }
+}
---
+++ src/main/java/user/collection/UserData.java
@@ -0,0 +1,16 @@
+package user.collection;
+
+import java.util.ArrayList;
+
+public class UserData {
+    private ArrayList<User> users;
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+}
+
